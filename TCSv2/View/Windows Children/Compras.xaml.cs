229ac88f7b9b08c05139214bfc5d05a8d040e45e
Diego Buezo <diegobuezo2016@gmail.com>
2019-07-24 01:18:11 -0600

    Se agrego el Boton de guardar la compra y tambien el boton de Nuevo Limpiar,etc

--- TCSv2/View/Windows Children/Compras.xaml.cs
+++ TCSv2/View/Windows Children/Compras.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,13 +22,157 @@ namespace TCSv2.View.Windows_Children
     /// </summary>
     public partial class Compras : Window
     {
+        SqlConnection sqlconnection;
         public Compras()
         {
+
             InitializeComponent();
+
             FechaSys.Text = DateTime.Now.ToString();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["TCSv2.Properties.Settings.TecnoCellConnectionString"].ConnectionString;
+            sqlconnection = new SqlConnection(connectionString);
+            ListarArticulos();
+            ListarProveedor();
+            ListarComprobantes();
+            double isv= Mostrar_ISC() * 100;
+            txtISV.Text = string.Format("{0}%",isv);
+            txtISV.IsEnabled = false;
         }
 
+        private void ListarComprobantes()
+        {
+            try
+            {
+                // El query ha realizar en la BD
+                string query = "SELECT * FROM Comprobante";
 
+                // SqlDataAdapter es una interfaz entre las tablas y los objetos utilizables en C#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlconnection);
+
+                using (sqlDataAdapter)
+                {
+                    // Objecto en C# que refleja una tabla de una BD
+                    DataTable tablaA = new DataTable();
+
+                    // Llenar el objeto de tipo DataTable
+                    sqlDataAdapter.Fill(tablaA);
+
+                    // ¿Cuál información de la tabla en el DataTable debería se desplegada en nuestro ListBox?
+                    cbComprobante.DisplayMemberPath = "Tipo";
+                    // ¿Qué valor debe ser entregado cuando un elemento de nuestro ListBox es seleccionado?
+                    cbComprobante.SelectedValuePath = "Id_Comprobante";
+                    // ¿Quién es la referencia de los datos para el ListBox (popular)
+                    cbComprobante.ItemsSource = tablaA.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void ListarArticulos()
+        {
+            try
+            {
+                // El query ha realizar en la BD
+                string query = "SELECT * FROM Articulo";
+
+                // SqlDataAdapter es una interfaz entre las tablas y los objetos utilizables en C#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlconnection);
+
+                using (sqlDataAdapter)
+                {
+                    // Objecto en C# que refleja una tabla de una BD
+                    DataTable tablaA = new DataTable();
+
+                    // Llenar el objeto de tipo DataTable
+                    sqlDataAdapter.Fill(tablaA);
+
+                    // ¿Cuál información de la tabla en el DataTable debería se desplegada en nuestro ListBox?
+                    cbArticulo.DisplayMemberPath = "Nombre";
+                    // ¿Qué valor debe ser entregado cuando un elemento de nuestro ListBox es seleccionado?
+                    cbArticulo.SelectedValuePath = "Id_Articulo";
+                    // ¿Quién es la referencia de los datos para el ListBox (popular)
+                    cbArticulo.ItemsSource = tablaA.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void ListarProveedor()
+        {
+            try
+            {
+                // El query ha realizar en la BD
+                string query = "SELECT * FROM Proveedor";
+
+                // SqlDataAdapter es una interfaz entre las tablas y los objetos utilizables en C#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlconnection);
+
+                using (sqlDataAdapter)
+                {
+                    // Objecto en C# que refleja una tabla de una BD
+                    DataTable tablaP = new DataTable();
+
+                    // Llenar el objeto de tipo DataTable
+                    sqlDataAdapter.Fill(tablaP);
+
+                    // ¿Cuál información de la tabla en el DataTable debería se desplegada en nuestro ListBox?
+                    cbProveedor.DisplayMemberPath = "Nombre";
+                    // ¿Qué valor debe ser entregado cuando un elemento de nuestro ListBox es seleccionado?
+                    cbProveedor.SelectedValuePath = "Id_Proveedor";
+                    // ¿Quién es la referencia de los datos para el ListBox (popular)
+                    cbProveedor.ItemsSource = tablaP.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private double Mostrar_ISC()
+        {
+            double ISV = 0;
+
+            try
+            {
+                string query = "SELECT ISV FROM ISV";
+
+                sqlconnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, sqlconnection);
+                // Reemplazar el parámetro con su valor respectivo
+            
+                sqlCommand.ExecuteNonQuery();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string x;
+                    x = reader["ISV"].ToString();
+                    ISV = double.Parse(x);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                sqlconnection.Close();
+            }
+
+
+
+            return ISV;
+        }
         private void BtnListado_Click(object sender, RoutedEventArgs e)
         {
 
@@ -140,6 +287,78 @@ namespace TCSv2.View.Windows_Children
         {
             //DragMove();
         }
-    
-}
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            txtStockInicial.Text = String.Empty;
+            txtNumero.Text = String.Empty;
+            txtPrecioCompra.Text = String.Empty;
+            txtPrecioVenta.Text = String.Empty;
+            cbArticulo.SelectedValue = null;
+            cbProveedor.SelectedValue = null;
+            cbComprobante.SelectedValue = null;
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                string query = "INSERT INTO Compra(Id_Proveedor, Id_Comprobante,Serie, Id_ISV,Estado) VALUES( @Id_Proveedor, @Id_Comprobante,@serie,@Id_ISV,@estado)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlconnection);
+
+                // Abrir la conexión
+                sqlconnection.Open();
+
+                // Reemplazar el parámetro con su valor respectivo
+
+                sqlCommand.Parameters.AddWithValue("@Id_Proveedor", cbProveedor.SelectedValue.ToString());
+                sqlCommand.Parameters.AddWithValue("@Id_Comprobante", cbComprobante.SelectedValue.ToString());
+                sqlCommand.Parameters.AddWithValue("@Id_ISV", "1");
+                sqlCommand.Parameters.AddWithValue("@serie", txtNumero.Text.ToString());
+                sqlCommand.Parameters.AddWithValue("@estado", "Emitido");
+             
+           
+               // sqlCommand.Parameters.AddWithValue("@fecha", DateTime.Now.ToString("DD/MM/YY"));
+                // Ejecutamos el query de inserción
+                // https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand.executescalar
+              
+
+
+                if (sqlCommand.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("La operación se ha completado correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("La operación No se ha completado correctamente");
+                }
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlconnection.Close();
+                // Actualizar el ListBox de Articulos
+                ListarArticulos();
+                ListarProveedor();
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            txtStockInicial.Text = String.Empty;
+            txtNumero.Text = String.Empty;
+            txtPrecioCompra.Text = String.Empty;
+            txtPrecioVenta.Text = String.Empty;
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
 }
